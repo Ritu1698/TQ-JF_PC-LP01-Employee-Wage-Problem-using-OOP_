31625b8c40efe02af8dff7e7d992c2055be9a70e
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 12:21:20 +0530

    [Rituparna]Added: Switch Case

--- src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -13,17 +13,26 @@ public class EmployeeWageProblem{
 
         System.out.println("Welcome to Employee Wage Computation Program"); 
 
-        double empCheck = Math.floor(Math.random() * 10) %3;
+        int empCheck = (int) Math.floor(Math.random() * 10) %3;
 
         if(empCheck == IS_FULL_TIME)
             System.out.println("Employee is Present");
         else
             System.out.println("Employee is Absent"); 
 
-        if(empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if(empCheck == IS_PART_TIME)
-            empHrs = 4;
+        switch(empCheck){
+            
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+
+            case  IS_FULL_TIME:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
+        }
 
         empWage = empHrs * EMPLOYEE_WAGE_PER_HOUR ;
         System.out.println("Employee wage is: "+ empWage);
