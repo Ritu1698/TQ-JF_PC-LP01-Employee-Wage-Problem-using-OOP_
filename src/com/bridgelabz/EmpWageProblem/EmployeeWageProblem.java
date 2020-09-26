46d3c90ddddb617300a3483b24eab7c162be8fff
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 12:18:20 +0530

    [Rituparna]Added: Calculating Daily Wage

--- src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -3,9 +3,13 @@ package com.bridgelabz.EmpWageProblem;
 public class EmployeeWageProblem{
 
     public static  final int IS_FULL_TIME = 1;
+    public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-	
+
+        int empHrs = 0;
+        int empWage = 0;
+
         System.out.println("Welcome to Employee Wage Computation Program"); 
 
         double empCheck = Math.floor(Math.random() * 10) %2;
@@ -15,6 +19,11 @@ public class EmployeeWageProblem{
         else
             System.out.println("Employee is Absent"); 
 
+        if(empCheck == IS_FULL_TIME)
+            empHrs = 8;
+
+        empWage = empHrs * EMPLOYEE_WAGE_PER_HOUR ;
+        System.out.println("Employee wage is: "+ empWage);
     }
 
 }
