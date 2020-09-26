cbb74942d56f8f116364ace9bef34fbfd86f400d
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 12:19:56 +0530

    [Rituparna]Added: Part Time Emp & Wage

--- src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -3,6 +3,7 @@ package com.bridgelabz.EmpWageProblem;
 public class EmployeeWageProblem{
 
     public static  final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
     public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
 
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class EmployeeWageProblem{
 
         System.out.println("Welcome to Employee Wage Computation Program"); 
 
-        double empCheck = Math.floor(Math.random() * 10) %2;
+        double empCheck = Math.floor(Math.random() * 10) %3;
 
         if(empCheck == IS_FULL_TIME)
             System.out.println("Employee is Present");
@@ -21,6 +22,8 @@ public class EmployeeWageProblem{
 
         if(empCheck == IS_FULL_TIME)
             empHrs = 8;
+        else if(empCheck == IS_PART_TIME)
+            empHrs = 4;
 
         empWage = empHrs * EMPLOYEE_WAGE_PER_HOUR ;
         System.out.println("Employee wage is: "+ empWage);
