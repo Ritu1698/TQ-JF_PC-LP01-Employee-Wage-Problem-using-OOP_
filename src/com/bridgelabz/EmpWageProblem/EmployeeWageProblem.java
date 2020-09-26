251f4a77b8f51e7b3646a6059dcf4edd8367adb3
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 12:26:02 +0530

    [Rituparna]Added: Monthy Wage Calculation With Condition

--- src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -5,16 +5,18 @@ public class EmployeeWageProblem{
     public static  final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public  static  final int TOTAL_WORKING_HOURS = 100;
 
     public static void main(String[] args) {
 
-        int empHrs = 0;
+        int empHrs = 0, totalEmpHrs = 0;
         int empWage = 0, totalEmpWage = 0;
+        int days = 0;
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+        while(days<NUM_OF_WORKING_DAYS && totalEmpHrs<=TOTAL_WORKING_HOURS)
         {
             int empCheck = (int) Math.floor(Math.random() * 10) %3;
 
@@ -39,10 +41,14 @@ public class EmployeeWageProblem{
             empWage = empHrs * EMPLOYEE_WAGE_PER_HOUR ;
             //System.out.println("Employee wage is: "+ empWage);
             totalEmpWage+=empWage;
+            totalEmpHrs+=empHrs;
+            days++;
         }
-        
-        
+
+
         System.out.println("Employee total wage is: "+ totalEmpWage);
+        System.out.println("Employee total hours is: "+ totalEmpHrs);
+        System.out.println("Total Days worked By the Employee:" + days);
         }
 
     }
