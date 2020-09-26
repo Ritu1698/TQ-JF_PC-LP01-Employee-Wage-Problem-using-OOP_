fa57143868c62d0566f45a2ed1dbcb09c034ed57
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-26 12:24:14 +0530

    [Rituparna]Added: Monthy Wage Calculation

--- src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -5,37 +5,44 @@ public class EmployeeWageProblem{
     public static  final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS=20;
 
     public static void main(String[] args) {
 
         int empHrs = 0;
-        int empWage = 0;
+        int empWage = 0, totalEmpWage = 0;
 
-        System.out.println("Welcome to Employee Wage Computation Program"); 
+        System.out.println("Welcome to Employee Wage Computation Program");
 
-        int empCheck = (int) Math.floor(Math.random() * 10) %3;
+        for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+        {
+            int empCheck = (int) Math.floor(Math.random() * 10) %3;
 
-        if(empCheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent"); 
+            /*if(empCheck == IS_FULL_TIME)
+                System.out.println("Employee is Present");
+            else
+                System.out.println("Employee is Absent");*/
 
-        switch(empCheck){
-            
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
+            switch(empCheck){
 
-            case  IS_FULL_TIME:
-                empHrs = 8;
-                break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
 
-            default:
-                empHrs = 0;
+                case  IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMPLOYEE_WAGE_PER_HOUR ;
+            //System.out.println("Employee wage is: "+ empWage);
+            totalEmpWage+=empWage;
+        }
+        
+        
+        System.out.println("Employee total wage is: "+ totalEmpWage);
         }
 
-        empWage = empHrs * EMPLOYEE_WAGE_PER_HOUR ;
-        System.out.println("Employee wage is: "+ empWage);
     }
-
-}
